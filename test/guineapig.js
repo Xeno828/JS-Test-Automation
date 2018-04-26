7796c40746b85e97b599c654d4a4cb88954c6668
Josh <jbruen17@gmail.com>
2018-04-26 17:59:50 +0100

    Updated framework

---
+++ test/guineapig.js
@@ -0,0 +1,12 @@
+const config = require('../nightwatch.conf.js');
+
+module.exports = {
+  'Guinea Pig Assert Title': function(browser) {
+    browser
+      .url('https://saucelabs.com/test/guinea-pig')
+      .waitForElementVisible('body')
+      .assert.title('I am a page title - Sauce Labs')
+      .saveScreenshot('guinea-pig-test.png')
+      .end();
+  }
+};
